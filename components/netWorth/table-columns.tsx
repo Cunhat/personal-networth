aa737d9869199bf6a564c0a3065fd35ab3296aaa
Tiago Cunha <tiago92cunha@gmail.com>
2023-12-29 14:41:02 +0000

    Fix layout fot big Ints

--- components/netWorth/table-columns.tsx
+++ components/netWorth/table-columns.tsx
@@ -25,6 +25,12 @@ export type NetWorthDataCategory = {
 
 export type NetWorthDataCategoryRow = NetWorthData | NetWorthDataCategory
 
+const RenderValue: React.FC<{ value: number | undefined }> = ({ value }) => (
+  <p>
+    {value !== undefined ? new Intl.NumberFormat().format(value) + "€" : "- -"}
+  </p>
+)
+
 export const columns: ColumnDef<NetWorthDataCategoryRow>[] = [
   {
     accessorKey: "account",
@@ -32,154 +38,76 @@ export const columns: ColumnDef<NetWorthDataCategoryRow>[] = [
     cell: (info) => <p>{info.getValue() as string}</p>,
     enableSorting: false,
   },
-  // {
-  //   accessorKey: "2022",
-  //   header: () => <p>2022</p>,
-  //   cell: (info) => <p>{info.getValue() as number}</p>,
-  //   enableSorting: false,
-  // },
   {
     accessorKey: "january",
     header: () => <p>January</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "february",
     header: () => <p>February</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "march",
     header: () => <p>March</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "april",
     header: () => <p>April</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "may",
     header: () => <p>May</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "june",
     header: () => <p>June</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "july",
     header: () => <p>July</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "august",
     header: () => <p>August</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "september",
     header: () => <p>September</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "october",
     header: () => <p>October</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "november",
     header: () => <p>November</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
   {
     accessorKey: "december",
     header: () => <p>December</p>,
-    cell: (info) => (
-      <p>
-        {info.getValue() !== undefined
-          ? new Intl.NumberFormat().format(info.getValue() as number) + " €"
-          : "- -"}
-      </p>
-    ),
+    cell: (info) => <RenderValue value={info.getValue() as number} />,
     enableSorting: false,
   },
 ]
